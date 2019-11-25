b45775e442aaa1fb67d41c2fe1dee01eddedac84
Cherry <ex00ia2432@gmail.com>
2019-11-25 15:56:04 +0900

    [clean]

--- s3/js/index.js
+++ s3/js/index.js
@@ -2,7 +2,7 @@
 let vm = new Vue({
     el: "#app", // Vue.jsを使うタグのIDを指定
     data: {
-    // Vue.jsで使う変数はここに記述する
+        // Vue.jsで使う変数はここに記述する
         mode: "login",
         submitText: "ログイン",
         toggleText: "新規登録",
@@ -14,14 +14,14 @@ let vm = new Vue({
         }
     },
     computed: {
-    // 計算した結果を変数として利用したいときはここに記述する
+        // 計算した結果を変数として利用したいときはここに記述する
     },
-    created: function() {
-    // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
+    created: function () {
+        // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
     },
     methods: {
-    // Vue.jsで使う関数はここで記述する
-        toggleMode: function(){
+        // Vue.jsで使う関数はここで記述する
+        toggleMode: function () {
             if (vm.mode === "login") {
                 vm.mode = "signup";
                 vm.submitText = "次へ";
@@ -40,48 +40,45 @@ let vm = new Vue({
                 fetch(url + "/bouquet/user/login", {
                     method: "POST",
                     body: JSON.stringify({
-                        "user_id":vm.user_id,
+                        "user_id": vm.user_id,
                         "password": vm.user.password
                     })
                 })
-                    .then(function(response) {
+                    .then(function (response) {
                         if (response.status === 200) {
                             return response.json();
                         }
                         // 200番以外のレスポンスはエラーを投げる
-                        return response.json().then(function(json) {
+                        return response.json().then(function (json) {
                             throw new Error(json.message);
                         });
                     })
-                    .then(function(json) {
-                    // レスポンスが200番で返ってきたときの処理はここに記述する
+                    .then(function (json) {
+                        // レスポンスが200番で返ってきたときの処理はここに記述する
                         let content = JSON.stringify(json, null, 2);
-                        //var content = JSON.stringify(json);
                         console.log(content);
                         console.log(json);
                         localStorage.setItem('token', json.token);
                         localStorage.setItem('user_id', vm.user.user_id);
-
                         // トップページへ遷移
                         location.href = "./calendar.html";
 
                     })
-                    .catch(function(err) {
-                        // レスポンスがエラーで返ってきたときの処理はここに記述する
+                    .catch(function (err) {
                         console.log(err);
                         return false;
                     });
             } else if (vm.mode === "signup") {
                 // 新規登録時
                 // 同じパスワードが入力された時の警告
-                if(vm.user.password !== vm.user.re_password){
+                if (vm.user.password !== vm.user.re_password) {
                     window.alert("正しいパスワードを入力してください");
                     // location.href = "./index.html";
                     return false;
                 }
                 // APIにPOSTリクエストを送る
                 console.log(vm.user);
-                fetch(url + "/bouquet/user/register",{
+                fetch(url + "/bouquet/user/register", {
                     method: "POST",
                     body: JSON.stringify({
                         "user_id": vm.user.user_id,
